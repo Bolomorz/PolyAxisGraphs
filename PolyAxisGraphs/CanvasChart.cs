9f73354988317bd58f8f686502a93c4927ed0c65
Dominik Schneider <Dominik.Schneider@wibizu.de>
2023-12-07 14:38:05 +0100

    main window

---
+++ PolyAxisGraphs/CanvasChart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolyAxisGraphs_Backend;
+
+namespace PolyAxisGraphs
+{
+    internal class CanvasChart
+    {
+        public PolyAxisGraph pag { get; set; }
+        public System.Windows.Controls.Canvas canvas { get; set; }
+
+        public CanvasChart(System.Windows.Controls.Canvas _canvas)
+        {
+            canvas = _canvas;
+            pag = new PolyAxisGraph(new Settings(@"..\..\..\Settings.txt"));
+        }
+
+        public void SetFilePath(string path)
+        {
+            pag.SetFilePath(path);
+            pag.ReadData();
+        }
+
+        public void SetLanguageFile(string file)
+        {
+            pag.SetLanguage(file);
+        }
+    }
+}
--- PolyAxisGraphs/MainWindow.xaml.cs
+++ PolyAxisGraphs/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace PolyAxisGraphs
 {
@@ -20,9 +22,50 @@ namespace PolyAxisGraphs
     /// </summary>
     public partial class MainWindow : Window
     {
+        CanvasChart cv { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            cv = new CanvasChart(canvas);
+            LoadControls();
+        }
+
+        private void LoadControls()
+        {
+            FontFamily ff = new FontFamily(Settings1.Default.usedfont);
+            rtbfile.FontFamily = ff;
+            rtbfile.FontSize = Settings1.Default.usedfontsize;
+            rtbfile.AppendText(cv.pag.settings.defaultlang.FindElement("tbopenfile"));
+            btopenfile.FontFamily = ff;
+            btopenfile.FontSize = Settings1.Default.usedfontsize;
+            btopenfile.Content = cv.pag.settings.defaultlang.FindElement("btopenfile");
+        }
+
+        private void OpenFileBtClick(object sender, RoutedEventArgs e)
+        {
+            string path = "";
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+            if (cv.pag.settings.initialdirectory is not null)
+            {
+                string id = System.IO.Path.GetFullPath(System.IO.Path.Combine(dir, cv.pag.settings.initialdirectory));
+                OpenFileDialog ofd = new OpenFileDialog()
+                {
+                    Title = "Open Data File",
+                    Multiselect = false,
+                    InitialDirectory = id,
+                    Filter = "TXT|*.txt|CSV|*.csv|XLSX|*.xlsx",
+                    FilterIndex = 0,
+                    RestoreDirectory = true
+                };
+
+                if(ofd.ShowDialog() == true)
+                {
+                    path = ofd.FileName;
+                    cv.SetFilePath(path);
+                    rtbfile.Document.Blocks.Clear();
+                    rtbfile.AppendText(path);
+                }
+            }
         }
     }
 }
